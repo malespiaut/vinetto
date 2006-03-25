2f5f1e448d10ca51b455fb62a65bbd96f6496c2c
rukin <rukin@f6a4722b-cf0f-0410-9ea0-aa9253d47ec1>
2006-03-25 10:41:03 +0000

    First import.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup (name='vinetto',
+    version='0.01pre-alpha',
+    py_modules=['???????????', '????????'],
+    description='vinetto : a forensics tool to examine Thumbs.db files.',
+    author='Michel Roukine',
+    author_email='rukin@users.sf.net',
+    url='http://vinetto.sourceforge.net/',
+    license='GNU GPL',
+    platforms='LINUX',
+)
