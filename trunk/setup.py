9520252c307ebd3f206614e1d70a4d3fd37347c1
rukin <rukin@f6a4722b-cf0f-0410-9ea0-aa9253d47ec1>
2006-03-27 14:31:45 +0000

    setup.py and vinetto tuning.

--- trunk/setup.py
+++ trunk/setup.py
@@ -2,6 +2,7 @@ from distutils.core import setup
 setup (name='vinetto',
     version='0.01pre-alpha',
     scripts=['vinetto'],
+    data_files=[('/usr/share/vinetto', ['res/header', 'res/huffman', 'res/quantization'])],
     description='vinetto : a forensics tool to examine Thumbs.db files.',
     author='Michel Roukine',
     author_email='rukin@users.sf.net',
