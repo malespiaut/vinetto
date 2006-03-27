f4d91571a4b50a12c8b8571e4e7b9aac89f65a22
rukin <rukin@f6a4722b-cf0f-0410-9ea0-aa9253d47ec1>
2006-03-27 13:55:16 +0000

    Updated setup.py

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup (name='vinetto',
     version='0.01pre-alpha',
-    py_modules=['???????????', '????????'],
+    scripts=['vinetto'],
     description='vinetto : a forensics tool to examine Thumbs.db files.',
     author='Michel Roukine',
     author_email='rukin@users.sf.net',
