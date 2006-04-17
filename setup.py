70636f05e8f301634a88f9e272e2deea93f0a475
rukin <rukin@f6a4722b-cf0f-0410-9ea0-aa9253d47ec1>
2006-04-17 21:50:05 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ from distutils.core import setup
 setup (name='vinetto',
     version='0.04pre-alpha',
     scripts=['vinetto'],
-    data_files=[('/usr/share/vinetto', ['res/header', 'res/huffman', 'res/quantization'])],
+    py_modules=['vinreport'],
+    data_files=[('/usr/share/vinetto', ['res/header', 'res/huffman', \
+                                        'res/quantization', \
+					'res/HtRepTemplate.html'])],
     description='vinetto : a forensics tool to examine Thumbs.db files.',
     author='Michel Roukine',
     author_email='rukin@users.sf.net',
